ec98e6155c2e07646b4909459bf491e1dd9d5e21
Wai <wai@Corei5-Coolermaster.(none)>
2011-10-23 23:53:46 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'e88e92e6c7d0f71e8c1355c9c586a471035fbde7d00a134084193d48e571e93fc9ef3c3ee8ffb0c630a97e2727b71aff46ecc46f15420f3951c8a5aa4bf232cf'
